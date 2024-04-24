4d8efb400219cd99b1854e71107c890dd877e384
AlxMen <alejandrocs402@gmail.com>
2024-04-24 13:00:45 +0100

    Implementacion de Morgan

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import morgan from 'morgan'
 import { corsConfig } from './config/cors'
 import { connectDB } from './config/db'
 import projectRoutes from './routes/projectRoutes'
@@ -11,9 +12,11 @@ connectDB()
 
 const app = express()
 app.use(cors(corsConfig))
-
+// Logging
+app.use(morgan('dev'))
+// Leer datos de formulario
 app.use(express.json())
-
+// Routes
 app.use('/api/projects', projectRoutes)
 
 
